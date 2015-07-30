02c8ac7e6fac2f74433dffaeff0e25c0b17f3f73
Yusheng <tamamali@icloud.com>
2015-07-30 15:46:08 +0800

    add destroy action for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    @book.destroy
+    # flash[:notice] = '已刪除'
+    redirect_to books_path, notice: '已成功刪除。'
+  end
+
   private
   def clean_params
     clean_params = params.require(:book).permit(:title, :content)
