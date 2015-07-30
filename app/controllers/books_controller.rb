328afd2d5fd6a28c28e081c892faeeb3c11c51f7
Yusheng <tamamali@icloud.com>
2015-07-30 15:39:19 +0800

    display book list in index and add books in new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,24 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
     @book = Book.new
+  end
+
+  def create
+    @book = Book.new(clean_params)
+    if @book.save
+      flash[:notice] = '成功新增書本'
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def clean_params
+    clean_params = params.require(:book).permit(:title, :content)
   end
 end
